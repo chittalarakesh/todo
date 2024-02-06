fc84aa5d02a60f1cf1b2897e35aeefb53e75820b
rakesh <rakeshpredera@gmail.com>
2024-02-06 12:46:23 +0530

    todos application is created

---
+++ todo.py
@@ -0,0 +1,19 @@
+class Todolist:
+    def __init__(self):
+        self.tasks=[]
+    def add.task(self,task):
+        self.tasks.append(task)
+    def display_tasks(self):
+        print("Tasks:")
+        for task in self.tasks:
+            print(task)
+
+    
+if __name__=="__main__":
+    todo_list=Todolist()
+    todo_list.add_task("complete work")
+    todo_list.add_task("completed work")
+    todo_list.add_task("writing the code")
+    todo_list.add_task("learning the git")
+
+
